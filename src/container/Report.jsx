01f328eeecde892e07d169ff2bdaeb20fce02c92
ZiaCodes <syed.nlp00@gmail.com>
2023-12-17 10:50:21 +0530

    feat: minor changes

--- src/container/Report.jsx
+++ src/container/Report.jsx
@@ -1,41 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import MainContainer from '../component/MainContainer'
 import VendorCard from '../component/vendor/VendorCard';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
 const Report = () => {
   const [callData, setCallData] = useState([]);
@@ -82,27 +47,10 @@ const Report = () => {
   },[changeData])
 
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => callData.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => callData.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
   return (
     <MainContainer>
 
       <div className='bg-lime-700 w-full h-96'>
-      <Bar options={options} data={data} />;
       </div>
       <div className='h-screen w-screen flex justify-center items-center flex-wrap gap-14 p-10'>
         {
