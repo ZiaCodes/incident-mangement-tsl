6bb6bea4941e4cb346eaa8877bb2254d27d4518e
ZiaCodes <syed.nlp00@gmail.com>
2023-12-25 14:23:32 +0530

    feat: instruction added

--- src/component/Table/TableLayout.jsx
+++ src/component/Table/TableLayout.jsx
@@ -149,10 +149,24 @@ const TableLayout = () => {
         </div>
     {
       !tableData?.length > 0 ? 
-      <p className='flex  justify-center items-center gap-2 mt-40 text-xl font-bold '>
-        <FcInfo className='text-2xl'/>
+      <div className='flex flex-wrap p-4 flex-col justify-center mt-8 items-center gap-8'>
+        <p className='flex  justify-center items-center gap-2 '>
+        <FcInfo className='text-xl'/>
         Upload an Excel Data to get insight and visualization.
-      </p>: 
+      </p>
+      <div className='flex p-2 gap-2 flex-col flex-wrap justify-center items-left'>
+        <h1 className='font-bold'>Instruction:</h1>
+        <p className='flex flex-wrap gap-2'>Step-1 : Download the  
+          <a 
+          className='text-red-600 shadow-none m-0 p-0 lowercase' 
+          href="./sample-excel-data.xlsx"
+          download
+          >sample Excel sheet</a>.</p>
+        <p>Step-2 : Fill your own data</p>
+        <p>step-3 : Upload the same Excel sheet</p>
+        <p><b>Note:</b> Please keep same sheetName i.e-Incident</p>
+      </div>
+      </div>: 
       <TableContainer>
       <TableHead/>
 
