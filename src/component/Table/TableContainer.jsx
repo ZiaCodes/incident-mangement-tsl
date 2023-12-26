4929b99e215f0ff05b6f90dd1ea9e44aa59bdb1d
ZiaCodes <syed.nlp00@gmail.com>
2023-12-26 10:44:42 +0530

    feat: minor changes

--- src/component/Table/TableContainer.jsx
+++ src/component/Table/TableContainer.jsx
@@ -3,8 +3,8 @@ const TableContainer = ({children}) => {
     <div className="flex flex-col m-4 overflow-x-auto ">
         <div className="sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                <div className="overflow-x-auto shadow-md">
-                        <table className="min-w-full text-left text-sm font-light ">
+                <div className="overflow-x-auto border p-4 shadow-sm">
+                        <table className=" min-w-full text-left text-sm font-light ">
                             {children}
                         </table>
                 </div>
